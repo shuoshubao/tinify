0e34798687ad94b407a153d54834c49195297bac
硕鼠宝 <759979885@qq.com>
2016-12-09 14:11:20 +0800

    去掉进度相关逻辑

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const tinify = require('tinify')
-tinify.key = 'XXIRu48sw8x3SMA4cA0NixJgib573DPX'
+tinify.key = 'KckuU929qtv_nPK_czL6HKfcAJO9FCKm'
 const fileList = []
 
 const getFileList = path => {
@@ -30,18 +30,11 @@ const targetFileList = fileList.filter(v => {
     return (v.endsWith('png') || v.endsWith('jpg')) && !v.includes('._')
 })
 
-const progress = {
-    all: targetFileList.length,
-    success: 0,
-    failure: 0,
-    failureList: []
-}
-
-if(progress.all === 0) {
+if(targetFileList.length === 0) {
     console.log(`未发现 .png 或 .jpg 类型的文件`)
     return
 }else {
-    console.log(`开始: 共 ${progress.all} 个文件`)
+    console.log(`开始: 共 ${targetFileList.length} 个文件`)
     console.time('耗时')
 }
 
@@ -50,6 +43,8 @@ if(progress.all === 0) {
 /**
  * 获取所有图片的大小
  */
+
+console.log(targetFileList)
 const targetFileListSize = []
 targetFileList.forEach(v => {
   const stats = fs.statSync(v)
@@ -65,22 +60,12 @@ console.log(`共${targetFileList.length}张图片, ${Math.ceil(targetFileListSiz
 return
 
 
+var iNow = 0;
+
 targetFileList.forEach((v, i) => {
     const source = tinify.fromFile(v)
     source.toFile(v, err => {
-        if (err) {
-            progress.failure++
-            progress.failureList.push(v)
-        } else {
-            progress.success++
-        }
-        console.log(`正在处理第 ${i}/${progress.all} 个文件, 成功 ${progress.success} 个, 失败 ${progress.failure} 个`)
-        if(i === progress.all - 1) {
-            console.log('结束')
-            console.timeEnd('耗时')
-            if(progress.failureList.length) {
-                console.log(['处理失败的文件:'].concat(progress.failureList).join('\n'))
-            }
-        }
+        iNow++
+        console.log(`进度: ${iNow}/${targetFileList.length} ${err ? '失败' : '成功'}: ${v}`)
     })
 })
